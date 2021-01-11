600a7c6f5bfc635076608e112ef367b9d347c1db
iray68 <yirayg@gmail.com>
2021-01-12 04:22:55 +0800

    Fix chunk file missing loading issue

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,15 @@
 import { render } from 'react-dom';
-import { MaskComponent } from '../dist/index';
-import { lazy, Suspense, CSSProperties } from 'react';
+import Picker, { MaskComponent } from '../dist/index';
+import { CSSProperties } from 'react';
 import 'normalize.css';
 import styles from './index.css';
 
+/*
+// TODO: Fix missing Chunk file issue
 const Picker = lazy(() =>
-  import(/* webpackChunkName: "Picker" */ '../dist/index')
+  import(/!* webpackChunkName: "Picker" *!/ '../dist/index')
 );
+*/
 
 interface TextProps {
   text: string;
@@ -35,64 +38,62 @@ const handleChange = (tag: string) => (number: number) =>
 
 render(
   <div className={styles.layout}>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Picker
-        height={300}
-        iconAdd={<Text text="+" />}
-        iconMinus={<Text text="-" />}
-        initCount={2019}
-        minCount={1970}
-        maxCount={2999}
-        preloadCount={8}
-        renderMask={MaskDateRender}
-        onChange={handleChange('year')}
-      />
-      <Picker
-        height={300}
-        initCount={6}
-        minCount={1}
-        maxCount={12}
-        preloadCount={8}
-        renderMask={MaskDateRender}
-        onChange={handleChange('month')}
-      />
-      <Picker
-        height={300}
-        initCount={6}
-        minCount={1}
-        maxCount={31}
-        preloadCount={8}
-        renderMask={MaskDateRender}
-        onChange={handleChange('day')}
-      />
+    <Picker
+      height={300}
+      iconAdd={<Text text="+" />}
+      iconMinus={<Text text="-" />}
+      initCount={2019}
+      minCount={1970}
+      maxCount={2999}
+      preloadCount={8}
+      renderMask={MaskDateRender}
+      onChange={handleChange('year')}
+    />
+    <Picker
+      height={300}
+      initCount={6}
+      minCount={1}
+      maxCount={12}
+      preloadCount={8}
+      renderMask={MaskDateRender}
+      onChange={handleChange('month')}
+    />
+    <Picker
+      height={300}
+      initCount={6}
+      minCount={1}
+      maxCount={31}
+      preloadCount={8}
+      renderMask={MaskDateRender}
+      onChange={handleChange('day')}
+    />
 
-      <Picker
-        initCount={6}
-        maxCount={23}
-        preloadCount={8}
-        onChange={handleChange('hour')}
-      />
-      <Picker
-        initCount={6}
-        maxCount={59}
-        preloadCount={8}
-        onChange={handleChange('minute')}
-      />
-      <Picker
-        className={styles.customPicker}
-        iconAdd={<Text className={styles.whiteButtonB} text="+" />}
-        iconMinus={<Text className={styles.whiteButtonT} text="-" />}
-        height={130}
-        initCount={6}
-        maxCount={59}
-        preloadCount={8}
-        renderMask={renderCustomMask(styles.gradientMask, {
-          height: 'calc(60% - 2px)',
-          top: 'calc(20% + 1px)'
-        })}
-        onChange={handleChange('second')}
-      />
-    </Suspense>
+    <Picker
+      initCount={6}
+      maxCount={23}
+      preloadCount={8}
+      onChange={handleChange('hour')}
+    />
+    <Picker
+      initCount={6}
+      maxCount={59}
+      preloadCount={8}
+      onChange={handleChange('minute')}
+    />
+    <Picker
+      className={styles.customPicker}
+      iconAdd={<Text className={styles.whiteButtonB} text="+" />}
+      iconMinus={<Text className={styles.whiteButtonT} text="-" />}
+      height={130}
+      initCount={6}
+      maxCount={59}
+      preloadCount={8}
+      renderMask={renderCustomMask(styles.gradientMask, {
+        height: 'calc(60% - 2px)',
+        top: 'calc(20% + 1px)'
+      })}
+      onChange={handleChange('second')}
+    />
   </div>,
   document.querySelector('#app')
 );
